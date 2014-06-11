5b2d69d3016919da9c66d843c48cee2111651767
Megaf <mmegaf@gmail.com>
2014-06-11 15:19:55 -0300

    Fixed alias

--- more_chests_cobble/init.lua
+++ more_chests_cobble/init.lua
@@ -1,4 +1,3 @@
-dofile(minetest.get_modpath("more_chests").."/aliases.lua")
 local function has_locked_chest_privilege(meta, player)
 	if player:get_player_name() ~= meta:get_string("owner") then
 		return false
@@ -92,3 +91,4 @@ minetest.register_craft({
 	}
 })
 
+minetest.register_alias("chests_0gb_us:cobble", "more_chests:cobble")
